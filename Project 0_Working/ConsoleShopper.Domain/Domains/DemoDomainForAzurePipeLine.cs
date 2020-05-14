47d3a2a9fbb6667d503705d7a39978a3e243b0ab
sulav <sulav.aryal@outlook.com>
2020-05-14 14:44:51 -0700

    adds property

--- Project 0_Working/ConsoleShopper.Domain/Domains/DemoDomainForAzurePipeLine.cs
+++ Project 0_Working/ConsoleShopper.Domain/Domains/DemoDomainForAzurePipeLine.cs
@@ -1,6 +1,7 @@
 namespace ConsoleShopper.Domain
 {
     public class DemoDomainForAzurePipeLine{
-             public string TestAzuerPipeLine { get; set; }               
+             public string TestAzuerPipeLine { get; set; }      
+	     public string NewTestAzuerPipeLine { get; set; }             
     }
 }
