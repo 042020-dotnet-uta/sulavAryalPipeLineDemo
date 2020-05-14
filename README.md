9c86a4d66fcb881549b742d3d87921601cf5e2e9
sulav <sulav.aryal@outlook.com>
2020-05-14 14:12:08 -0700

    removed everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# sulavAryalPipeLineDemo
-Pipeline demo repo
--- pipeLineMvcDemo/Models/PipeLine.cs
+++
@@ -1,9 +0,0 @@
-namespace pipelineMvcDemo.Models
-{
-	public class Pipeline
-	{
-		public int Length {get;set;}
-		public int Width {get;set;}
-		public int Throughput {get;set;}
-	}
-}
